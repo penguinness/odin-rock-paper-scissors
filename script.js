3168503a4833aae63fbe3bc43818c747e6514942
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-09 21:32:23 -0400

    finished rock paper scissors

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function getComputerChoice() {
   const randomInt = Math.floor(Math.random() * 3);
-  console.log(randomInt);
+  //   console.log(randomInt);
   if (randomInt === 0) {
     return 'Rock';
   } else if (randomInt === 1) {
@@ -30,3 +30,35 @@ function getHumanChoice() {
 let humanScore = 0;
 
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 'paper' && computerChoice === 'rock') {
+    humanScore++;
+    alert('You win! Paper beats Rock.');
+  } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    humanChoice++;
+    alert('You win! Rock beats Scissors.');
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    humanChoice++;
+    alert('You win! Scissors beats Paper.');
+  } else if (computerChoice === 'paper' && humanChoice === 'rock') {
+    computerScore++;
+    alert('You lose! Paper beats Rock.');
+  } else if (computerChoice === 'rock' && humanChoice === 'scissors') {
+    computerScore++;
+    alert('You lose! Rock beats Scissors.');
+  } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+    computerScore++;
+    alert('You lose! Scissors beats Paper.');
+  } else {
+    alert(`It's a draw!`);
+  }
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice().toLowerCase();
+    const computerSelection = getComputerChoice().toLowerCase();
+    playRound(humanSelection, computerSelection);
+  }
+}

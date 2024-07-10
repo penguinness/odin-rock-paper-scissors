b8fb25ccb82e2a7ee397359dc6819db3633cc347
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-09 22:37:43 -0400

    fixed exception handling

--- script.js
+++ script.js
@@ -13,17 +13,19 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt(`Please type "Rock", "Paper", or "Scissors".`);
-  if (humanChoice.toLowerCase() === 'rock') {
-    return 'Rock';
-  } else if (humanChoice.toLowerCase() === 'paper') {
-    return 'Paper';
-  } else if (humanChoice.toLowerCase() === 'scissors') {
-    return 'Scissors';
-  } else {
-    alert(
-      `Invalid entry! Please enter a valid move - "Rock", "Paper, or "Scissors".`
-    );
+  while (true) {
+    let humanChoice = prompt(`Please type "Rock", "Paper", or "Scissors".`);
+    if (humanChoice.toLowerCase() === 'rock') {
+      return 'Rock';
+    } else if (humanChoice.toLowerCase() === 'paper') {
+      return 'Paper';
+    } else if (humanChoice.toLowerCase() === 'scissors') {
+      return 'Scissors';
+    } else {
+      alert(
+        `Invalid entry! Please enter a valid move - "Rock", "Paper, or "Scissors".`
+      );
+    }
   }
 }
 
